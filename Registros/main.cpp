932f330869bf4c8220c554d4c5c4a0f8c400aeed
Gustavo <omarperez1410@gmail.com>
2022-01-05 17:42:12 -0600

    Registros

---
+++ Registros/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+struct Persona{
+    char nombre [30]; //campos
+    char apellido [30];
+    int edad;
+};
+
+int main(){
+    Persona p;
+
+    cout<<"Ingrese el nombre: "<<endl;
+    cin>>p.nombre;
+    cout<<"Ingrese el apellido: "<<endl;
+    cin>>p.apellido;
+    cout<<"Ingrese la edad: "<<endl;
+    cin>>p.edad;
+    cout<<"Mostrando datos: "<<endl;
+    cout<<p.nombre<<endl;
+    cout<<p.apellido<<endl;
+    cout<<p.edad<<endl;
+
+    return 0;
+}
